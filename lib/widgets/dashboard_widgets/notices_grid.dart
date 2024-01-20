dc4ca219616cc76474d4d80eaeaeee300c7821ff
sanaullah-dev <sana.dev11211@gmail.com>
2024-01-20 20:47:51 +0500

    done

--- lib/widgets/dashboard_widgets/notices_grid.dart
+++ lib/widgets/dashboard_widgets/notices_grid.dart
@@ -53,6 +53,7 @@ class _NoticesGridState extends State<NoticesGrid> {
                       child: GridView.builder(
                           physics: const NeverScrollableScrollPhysics(),
                           // itemCount: posts.postId.length <= 4 ? posts.postId.length : 4,
+                          // SANA changes
                           itemCount: widget.posts!.length <= 4
                               ? widget.posts!.length
                               : 4,
